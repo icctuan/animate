390963e3e0d591eb0830a1f6c290fdd9123a4bdf
icctuan <601838356@qq.com>
2023-05-05 18:07:30 +0800

    chore: 项目搭建

---
+++ src/components/Animation/index.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+import { FC } from 'react'
+
+const Animation:FC<any> = () => {
+  return <div className="wave">
+    波浪线
+  </div>
+}
+
+export default Animation
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ConfigEnv, UserConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+
+  return {
+    envPrefix: 'VITE_',
+    plugins: [react()],
+    server: {
+      // host: 'dev.icctuan.animate.cn',
+      port: 5564,
+      open: true
+    }
+  }
+}
